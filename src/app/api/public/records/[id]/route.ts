a592e29797ef276cf4ad7fbe2ee49c4f16b48eba
Wesley Fernandes <wesleyintelligence@protonmail.com>
2024-11-24 01:29:42 -0300

    fix: new argument

--- src/app/api/public/records/[id]/route.ts
+++ src/app/api/public/records/[id]/route.ts
@@ -1,7 +1,9 @@
 import database from '@/lib/prisma';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-export async function GET(request:NextRequest, { params }: { params: { id: string } }
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
 ) {
   try {
     const { id } = params;
